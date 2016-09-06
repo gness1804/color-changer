ff9b1d8a23012700275f6ee0a9d598668d2fe372
gness1804 <graham.nessler@gmail.com>
2016-09-06 13:55:08 -0600

    got 3 random color picker working except for assigning to rgb

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
   $("#random-button").click(function () {
-    numberGenerate();
+    assignColor();
   });
   $("#reset-button").click(function () {
     reset();
   });
 
 
-}); // end of jQuery body
+});
 
+function assignColor() {
+  var a = randomRGBVal();
+  var b = randomRGBVal();
+  var c = randomRGBVal();
+  console.log(a, b, c);
+}
 
-// function numberGenerate (min, max) {
-//   var a =
-//
-//   var result = Math.random();
-//   assignColor(result);
-// }
 
 function randomRGBVal (min, max) { //returns random number between 0 and 255
   min = 0;
@@ -23,19 +23,19 @@ function randomRGBVal (min, max) { //returns random number between 0 and 255
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-console.log(randomRGBVal());
+
 
 // if number is within a certain range, generate a color
 
-function assignColor (number) {
-  var color = "rgb(a, b, c)";
-  var a;
-  var b;
-  var c;
-
-  $("html").css("backgroundColor", color);
-}
-
-function reset () {
-  $("html").css("backgroundColor", "white");
-}
+// function assignColor (number) {
+//   var color = "rgb(a, b, c)";
+//   var a;
+//   var b;
+//   var c;
+//
+//   $("html").css("backgroundColor", color);
+// }
+//
+// function reset () {
+//   $("html").css("backgroundColor", "white");
+// }
